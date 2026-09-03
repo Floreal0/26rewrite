5791c7f2fe619a552e78b3222fea4698e832c415
霁月 <51904772+Floreal0@users.noreply.github.com>
2026-09-03 22:57:13 +0800

    Delete 语法练习 directory

--- 语法练习/9.16-9.4.1定义并使用类方法.py
+++
@@ -1,8 +0,0 @@
-class SmplClass():
-    def info(self):
-        print('python')
-    def mycacl(self,x,y):
-        return(x+y)
-sc=SmplClass()
-sc.info()
-print(sc.mycacl(3,4))
--- 语法练习/os使用&builtins.py
+++
@@ -1,6 +0,0 @@
-import os
-#......
-a=dir(__builtins__)
-print(a)
-
-os.system('pause')
